daa1a2388c7800131740a4abefef6df624a9ebe9
Milos87Visnjic <91454845+Milos87Visnjic@users.noreply.github.com>
2023-07-26 14:27:11 +0200

    Add files via upload

---
+++ Test-Automation-JavaLoops/src/main/java/infinite_forLoop.java
@@ -0,0 +1,9 @@
+public class infinite_forLoop {
+    public static void main(String[] args) {
+        //infinite for loop
+        for (;;){
+            System.out.println("Good");
+        }
+
+    }
+}
